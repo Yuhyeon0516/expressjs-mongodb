f1707ff6f063459b7206f99ee52fb3c928165d0e
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-29 17:21:47 +0900

    CRUD Product Read 기능 추가

--- routes/products.router.js
+++ routes/products.router.js
@@ -2,7 +2,8 @@ const express = require("express");
 const productsController = require("../controllers/products.controller");
 const productsRouter = express.Router();
 
-// productsRouter.post("/", productsController.createProduct);
 productsRouter.post("/", productsController.createProduct);
+productsRouter.get("/", productsController.getProducts);
+productsRouter.get("/:productId", productsController.getProductById);
 
 module.exports = productsRouter;
